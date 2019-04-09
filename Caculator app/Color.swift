bf84ee746b6d2b2a677784f1c7666bdf902ddb5a
cuonghx <cuonghx@cuonghxs-MacBook-Pro.local>
2019-04-09 10:37:36 +0700

    added source code app caculator

---
+++ Caculator app/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Caculator app
+//
+//  Created by cuonghx on 4/9/19.
+//  Copyright © 2019 cuonghx. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    static let orange = UIColor(red: 213/256, green: 117/256, blue: 44/256, alpha: 1)
+    static let gray = UIColor(red: 214/256, green: 214/256, blue: 214/256, alpha: 1)
+}
